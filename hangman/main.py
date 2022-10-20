a4418586ecdc19efb81ddc6a68d68cb6459decfe
omar aguinaga <omar_aguinaga@icloud.com>
2022-10-20 11:36:46 -0500

    Update hangman project

--- hangman/main.py
+++ hangman/main.py
@@ -12,7 +12,7 @@ for current_answer_character in answer:
     if re.search("^[A-Z]$", current_answer_character):
         answer_guessed.append(False)
     else:
-        answer_guessed.appen(True)
+        answer_guessed.append(True)
 
 # Game logic.
 num_of_incorrect_guesses = 5
@@ -25,3 +25,25 @@ letters_guessed = []
 while current_incorrect_guesses < num_of_incorrect_guesses and False in answer_guessed:
     # Display game status.
     print(f"Number of incorrect guesses remaining: {num_of_incorrect_guesses - current_incorrect_guesses}")
+
+    print("Letters guessed: ", end="")
+
+    for current_letters_guessed in letters_guessed:
+        print(current_letters_guessed, end=" ")
+
+    print()
+
+    # Display puzzle board.
+    for current_answer_index in range(len(answer)):
+        if answer_guessed[current_answer_index]:
+            print(answer[current_answer_index], end="")
+        else:
+            print("_", end="")
+    
+    print()
+
+    # Let the user guess a letter.
+    letter = input("Enter a letter: ")
+
+    # Check if user entered a valid letter.
+    if 
