2c1c187594c127d43c93e9be914aedffd91cfdec
omar11500 <omar_aguinaga@icloud.com>
2022-11-22 11:42:59 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (777, 'Estamos Bien', '2019-05-30 11:11:11');
+
+--UPDATE sandbox SET int32_value = -1024620 WHERE id = 100;
+
+--DELETE FROM sandbox WHERE id = 84;
+
+SELECT * FROM sandbox;
